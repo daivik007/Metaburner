d8fa8d6f9b4c8d924c11ae53243ecc4e3507499f
Daivik Trivedi <dntrivedi2006@gmail.com>
2025-04-17 22:28:06 +0530

    dependencies addde

---
+++ setup_dependencies.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Installing FFmpeg using winget...
+winget install -e --id Gyan.FFmpeg
+
+echo Installing ffmpeg-progressbar-cli globally with npm...
+npm install --global ffmpeg-progressbar-cli
+
+echo All dependencies installed!
+pause
